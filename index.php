d75a47372990d15eb8ec117e2de15d822c65e4cf
SoulSndr <106753043+SoulSndr@users.noreply.github.com>
2024-12-03 18:53:35 +0100

    fixed warnings

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 	<head>
 		<link rel="stylesheet" href="style/style.css">
 		<meta charset="utf-8">
 		<script src="script/responsive.js" type="module"></script>
-	</head>
+        <title>AlcoolEcoute</title>
+    </head>
 	<body>
 		<header>
-			<div><img src="./media/logo_AE.png" id="logo"/></div>
+			<div><img src="./media/logo_AE.png" id="logo" alt="logoAlcoolEcoute"/></div>
 			<h1>ALCOOL ECOUTE</h1>
 			<h2>Joie & Santé</h2>
 
-			<img src="./media/mobileNav.png" id="menu">
+			<img src="media/logoMenuDeroulant.png" alt="logoMenuDeroulant" id="menu">
 		</header>
 		<p>iduzahiduazhiduh</p>
 		<p>iduzahiduazhiduh</p>
