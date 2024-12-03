0094ac7e0d02964e730adb20e75c8178425e27e5
SoulSndr <106753043+SoulSndr@users.noreply.github.com>
2024-12-03 18:58:27 +0100

    modularizing header

--- index.php
+++ index.php
@@ -8,13 +8,8 @@
         <title>AlcoolEcoute</title>
     </head>
 	<body>
-		<header>
-			<div><img src="./media/logo_AE.png" id="logo" alt="logoAlcoolEcoute"/></div>
-			<h1>ALCOOL ECOUTE</h1>
-			<h2>Joie & Santé</h2>
+        <?php require_once "includes/header.html"?>
 
-			<img src="media/logoMenuDeroulant.png" alt="logoMenuDeroulant" id="menu">
-		</header>
 		<p>iduzahiduazhiduh</p>
 		<p>iduzahiduazhiduh</p>
 		<p>iduzahiduazhiduh</p>
