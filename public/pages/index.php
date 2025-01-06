3453fcdea1661e22d95bae404c91ecd6fd43430e
SoulSndr <oskanianvictor@yahoo.com>
2025-01-06 21:24:45 +0100

    rajout de index et utilisation de includes

---
+++ public/pages/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Accueil - Alcool Ecoute</title>
+  <link rel="stylesheet" href="../style/style.css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+
+  <script src="../script/navbar-mobile.js" type="module" defer></script>
+</head>
+
+<body>
+
+<?php
+  $pageActuelle = 'index'; // Définit la page active pour css
+  require_once "../includes/header.php"
+?>
+
+
+<?php require_once "../includes/footer.html"?>
+
+</body>
+</html>
+
