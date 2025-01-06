6e6425cdbd6c50be927a602a0a31b12cd2f3791d
Tiss <bludikat@gmail.com>
2025-01-06 23:44:58 +0100

    Fix la positionn de la navbar (elle est encore buggé) + l'icone de la navbar est cachée en grand écran

--- public/pages/index.php
+++ public/pages/index.php
@@ -17,8 +17,53 @@
   require_once "../includes/navbar.html" ?>
 </header>
 
-<?php require_once "../includes/footer.html"?>
+<div id="page">
 
+    <article>
+        <h2>L’alcool est-il mauvais pour la santé ?</h2>
+        <p>
+            La consommation de l’alcool peut-être très dangeureuse et provoquer des dommages
+            importants sur la santé.<br>A long terme, elle augmente le risque de développer un cancer,
+            une maladie du foie, des maladies cardiovasculaires et des troubles sur le système nerveux.<br>
+            L’alcool augmente le risque d’accident sur la route et la violence familiale, intime et sexuelle.
+        </p>
+    </article>
+
+    <article class="row justify-content-center">
+        <div class="col-12 col-md-6">
+            <h2>Tout savoir sur l'alcool</h2>
+            <ul>
+                <li>Sortir de l’addiction</li>
+                <li>Les témoignages</li>
+                <li>Comprendre l’addiction</li>
+                <li>Quelles complications ?</li>
+                <li>Addiction et société</li>
+            </ul>
+        </div>
+        <div class="col-12 col-md-6 justify-content-center">
+            <img src="../media/imgnon1.png">
+        </div>
+    </article>
+
+    <article class="row justify-content-center">
+        <h2>Qui sommes-nous ?</h2>
+        <div class="col-12 col-md-6 justify-content-center">
+            <img src="../media/rnoiquestion.png">
+        </div>
+        <div class="col-12 col-md-6">
+            <p>
+                Fédération Nationale Joie et Santé dite « Alcool Ecoute FNJS »
+            </p>
+            <p>
+                Groupement d’Associations d’Aide aux personnes en Difficulté avec l’Alcool.<br>
+                Fondée en 1964, composée d’Associations régies par la loi du 1er juillet 1901.
+            </p>
+        </div>
+    </article>
+
+</div>
+
+<?php require_once "../includes/footer.html"?>
 </body>
 </html>
 
