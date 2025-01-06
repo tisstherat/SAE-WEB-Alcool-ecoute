625b45fb28392872d9e36abc2170b3e9c58c17d0
SoulSndr <oskanianvictor@yahoo.com>
2025-01-06 21:25:40 +0100

    rajout de index et utilisation de includes

--- public/includes/header.php
+++ public/includes/header.php
@@ -1,9 +1,11 @@
 <header class="row">
-    <div class="col-2"><img src="../media/logo_AE.png" id="logo"/></div>
-    <div class="col-8 container">
-        <h1 class="col">ALCOOL ECOUTE</h1>
-        <h2 class="col">Joie & Santé</h2>
-    </div>
-    <div class="col-2"><img src="../media/mobileNav.png" id="menu"></div>
+  <div class="col-2"><img src="../media/logo_AE.png" id="logo" alt="Logo Alcool Ecoute"/></div>
+  <div class="col-8 container">
+    <h1 class="col">ALCOOL ECOUTE</h1>
+    <h2 class="col">Joie & Santé</h2>
+  </div>
+  <div class="col-2"><img src="../media/mobileNav.png" id="menu" alt="Menu"></div>
+  <div class="login"></div>
 
+  <?php require_once "navbar.html"?>
 </header>
