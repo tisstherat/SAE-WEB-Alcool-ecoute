9787514804ddc7952c7d2554d2e26da824f32f54
SoulSndr <oskanianvictor@yahoo.com>
2025-01-07 00:16:37 +0100

    ajout de stats.php

---
+++ public/pages/stats.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php $pageActuelle = 'Formulaire'; // Définit la page active pour css navbar
+require_once "../includes/head.html"?>
+
+<body>
+<?php require_once "../includes/header.html" ?>
+
+<p>contenu stats</p>
+
+<?php require_once "../includes/footer.html"?>
+</body>
+</html>
