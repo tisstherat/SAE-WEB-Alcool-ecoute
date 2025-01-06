6c343360d12c34647410af5f151ffc225f48f08d
SoulSndr <oskanianvictor@yahoo.com>
2025-01-06 22:50:37 +0100

    includes pour head et modif header

--- public/pages/historique.php
+++ public/pages/historique.php
@@ -1,21 +1,13 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Historique - Alcool Ecoute</title>
-        <link rel="stylesheet" href="../style/style.css">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
 
-        <script src="../script/navbar-mobile.js" type="module" defer></script>
-    </head>
+    <?php $pageActuelle = 'Historique'; // Définit la page active pour css
+    require_once "../includes/head.html"?>
+
     <body class="historique-page">
 
-      <header class="row">
         <?php require_once "../includes/header.html" ?>
-        <?php $pageActuelle = 'historique'; // Définit la page active pour css
-        require_once "../includes/navbar.html" ?>
-      </header>
+        <?php require_once "../includes/navbar.html" ?>
 
         <div id="page">
 
