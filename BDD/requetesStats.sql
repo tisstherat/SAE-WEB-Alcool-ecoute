3241d09610213291010d34198e6bfe0048bf1dae
Tiss <bludikat@gmail.com>
2025-01-08 23:17:01 +0100

    requetesStats.sql finie

--- BDD/requetesStats.sql
+++ BDD/requetesStats.sql
@@ -1,3 +1,22 @@
-SELECT age, COUNT(*) AS 'nombre' FROM enquete GROUP BY age ORDER BY 1;
-SELECT emploi, COUNT(*) AS 'nombre' FROM enquete GROUP BY emploi ORDER BY 2 DESC;
-SELECT lieu_de_vie, COUNT(*) AS 'nombre' FROM enquete GROUP BY lieu_de_vie ORDER BY 2 DESC;
+#TOTAL USERS
+SELECT COUNT(*) AS total FROM enquete;
+
+#QUI A REPONDU
+SELECT MIN(age) AS plusJeune, MAX(age) AS plusAge, ROUND(AVG(age)) AS moyenne FROM enquete;
+#QUI A REPONDU SUITE
+SELECT region, COUNT(*) AS nombre FROM enquete GROUP BY region ORDER BY 1;
+
+#LIEU DE VIE
+SELECT lieu_de_vie, COUNT(*) AS nombre FROM enquete GROUP BY lieu_de_vie ORDER BY 1;
+
+#LDV CDAPH
+SELECT lieu_de_vie_CDAPH, COUNT(*) AS nombre FROM enquete GROUP BY lieu_de_vie_CDAPH ORDER BY 1;
+
+#LDV CHOIX
+SELECT lieu_de_vie_choix, COUNT(*) AS nombre FROM enquete GROUP BY lieu_de_vie_choix ORDER BY 1;
+
+#ACTIVITE PRO SOCIALE
+SELECT emploi, COUNT(*) AS nombre FROM enquete GROUP BY emploi ORDER BY 1;
+
+#BESOIN SOUTIEN
+SELECT besoin, COUNT(*) AS nombre FROM enquete GROUP BY besoin ORDER BY 1;
