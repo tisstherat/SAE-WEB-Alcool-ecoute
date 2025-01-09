0c9fa5c6440c97b64b91209374808f4a5b29e601
SoulSndr <oskanianvictor@yahoo.com>
2025-01-09 04:45:17 +0100

    ajout admin pour la démo

--- BDD/resetDataPourDemo.sql
+++ BDD/resetDataPourDemo.sql
@@ -5,3 +5,10 @@ TRUNCATE TABLE users;
 TRUNCATE TABLE admin;
 TRUNCATE TABLE enquete;
 SET FOREIGN_KEY_CHECKS = 1; -- Réactive les contraintes de clé étrangère
+
+INSERT INTO users(id, nom, prenom, email, password, created_at)
+VALUES (1, 'Administra', 'Teur', 'admin@alcoolecoute.fr', '$2y$10$5f04edizS.14kAUSWvyaaumfQeGU1KucEJT7XvQCZ.FnTsTDgOQx.', CURRENT_DATE);
+
+# administrateurs
+INSERT INTO admin(id_users)
+VALUES (1);
